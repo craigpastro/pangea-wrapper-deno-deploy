26e8a60bbb72db5b7a61e08a7e7e1c6c044b6573
Craig Pastro <pastro.craig@gmail.com>
2023-05-14 08:23:59 -0700

    Change endpoint to GET (#9)

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,49 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, isHttpError } from "https://deno.land/x/oak/mod.ts";
 
 const responseTimeHeader = "X-Response-Time";
 
+// The first middleware is the outer most.
 export function registerMiddleware(app: Application) {
+  // request and response logger
   app.use(async (ctx, next) => {
     await next();
-    const rt = ctx.response.headers.get(responseTimeHeader);
-    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+
+    const msg = {
+      req: {
+        url: ctx.request.url.href,
+        method: ctx.request.method,
+        ip: ctx.request.ip,
+      },
+      resp: { status: ctx.response.status },
+      rt: ctx.response.headers.get(responseTimeHeader),
+    };
+    console.log(msg);
   });
 
+  // request timing
   app.use(async (ctx, next) => {
     const start = Date.now();
     await next();
-    const ms = Date.now() - start;
-    ctx.response.headers.set(responseTimeHeader, `${ms}ms`);
+    const rt = Date.now() - start;
+
+    console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}ms`);
+    ctx.response.headers.set(responseTimeHeader, `${rt}ms`);
+  });
+
+  // error handler
+  app.use(async (ctx, next) => {
+    try {
+      await next();
+    } catch (err) {
+      console.log(err);
+
+      if (isHttpError(err)) {
+        ctx.response.status = err.status;
+      } else {
+        ctx.response.status = 500;
+      }
+
+      ctx.response.body = JSON.stringify({ error: err.message });
+    }
   });
 }
