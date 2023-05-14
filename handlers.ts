502f3cfe7021bfa00a232a3a5f551e6fa43c1689
Craig Pastro <pastro.craig@gmail.com>
2023-05-14 09:09:03 -0700

    Add isVpn (#10)

--- handlers.ts
+++ handlers.ts
@@ -7,8 +7,11 @@ const pangeaDomain = Deno.env.get("PANGEA_DOMAIN");
 const pangeaToken = Deno.env.get("PANGEA_TOKEN");
 
 export function frontend(ctx: Context) {
-  ctx.response.body =
-    `A wrapper for Pangea's Embargo Service. Try /embargo?countryCode=<countryCode>"} to check that countries embargo status.`;
+  ctx.response.body = `A wrapper for Pangea's Embargo Service. Try:
+    - /embargo?countryCode=<countryCode> to check that countries embargo status.
+    - /is-vpn?ip=<ip> to check whether a given ip originates from a VPN. No query parameter should check your ip.
+    
+See https://pangea.cloud for more information.`;
 }
 
 export async function checkEmbargoStatus(ctx: Context) {
@@ -17,7 +20,8 @@ export async function checkEmbargoStatus(ctx: Context) {
     ctx.throw(400, "please set the countryCode query parameter");
   }
 
-  const cachedResult = ttl.get(countryCode);
+  const cacheKey = `embargo-${countryCode}`;
+  const cachedResult = ttl.get(cacheKey);
   if (cachedResult) {
     ctx.response.body = cachedResult;
     return;
@@ -40,7 +44,43 @@ export async function checkEmbargoStatus(ctx: Context) {
     ctx.throw(500, "error calling pangea");
   }
 
-  ttl.set(countryCode, result, 86400); // cache for one day
+  ttl.set(cacheKey, result, 86400); // cache for one day
 
   ctx.response.body = result;
+}
+
+export async function checkVpnStatus(ctx: Context) {
+  let ip = ctx.request.url.searchParams.get("ip");
+  if (!ip) {
+    ip = ctx.request.ip;
+  }
+
+  const cacheKey = `is-vpn-${ip}`;
+  const cachedResult = ttl.get(cacheKey);
+  if (cachedResult) {
+    ctx.response.body = cachedResult;
+    return;
+  }
+
+  const body = `{"provider":"digitalelement", "ip":"${ip}"}`;
+  const resp = await fetch(`https://ip-intel.${pangeaDomain}/v1/vpn`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${pangeaToken}`,
+    },
+    body,
+  });
+
+  const jsonData = await resp.json();
+  const result = jsonData.result;
+  if (!result) {
+    console.log("error calling pangea:", jsonData);
+    ctx.throw(500, "error calling pangea");
+  }
+
+  const respBody = { ip, isVpn: result.data.is_vpn };
+  ttl.set(cacheKey, respBody, 43200); // cache for half a day
+
+  ctx.response.body = respBody;
 }
